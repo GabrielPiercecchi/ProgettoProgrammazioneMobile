05ba75b0b299fbbbeae8b9da04765a35fdb89e07
GabrielP <S1097612@studenti.univpm.it>
2023-04-19 18:00:37 +0200

    Aggiunto metodo per il controllo della password inserita in ForgotPassword.kt

--- app/src/main/java/com/example/myandroidapplication/view/ForgotPassword.kt
+++ app/src/main/java/com/example/myandroidapplication/view/ForgotPassword.kt
@@ -8,8 +8,12 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myandroidapplication.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.auth.User
 
 class ForgotPassword : AppCompatActivity() {
 
@@ -33,15 +37,52 @@ class ForgotPassword : AppCompatActivity() {
 
 
         btnReset.setOnClickListener {
-            val sPassword = editBox.text.toString()
-            mAuth.sendPasswordResetEmail(sPassword)
-                .addOnSuccessListener {
-                 Toast.makeText(this, "Please check your email", Toast.LENGTH_SHORT). show()
-                }
-                .addOnCompleteListener {
-                    Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
-                }
+            val emailRPassword = editBox.text.toString()
+            checkEmail(emailRPassword)
 
+//            mAuth.sendPasswordResetEmail(emailRPassword)
+//                .addOnSuccessListener {
+//                    Toast.makeText(this, "Please check your email", Toast.LENGTH_SHORT). show()
+//                }
+//                .addOnCompleteListener {
+//                    Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+//                }
         }
+    }
+    private fun checkEmail(emailRPassword: String){
+        mAuth.sendPasswordResetEmail(emailRPassword)
+
+        //check in an user is signed in
+        val databaseRef = FirebaseDatabase.getInstance().getReference("user")
+        val query = databaseRef.orderByChild("email").equalTo(emailRPassword.toString())
+
+        query.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    // L'email è presente nel database
+                    mAuth.sendPasswordResetEmail(emailRPassword)
+                        .addOnSuccessListener {
+                            Toast.makeText(this@ForgotPassword, "Please check your email",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                        .addOnCompleteListener {
+                            Toast.makeText(this@ForgotPassword, it.toString(),
+                                Toast.LENGTH_SHORT).show()
+                        }
+                } else {
+                    // L'email non è presente nel database
+                    Toast.makeText(this@ForgotPassword, "The email doesn't exist",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                // Errore nel recupero dei dati dal database
+                Toast.makeText(
+                    this@ForgotPassword,
+                    "Error: $error",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        })
     }
 }
