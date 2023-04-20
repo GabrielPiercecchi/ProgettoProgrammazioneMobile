3916eb497fd74e392a31e333ef3e4f603126420b
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2023-04-20 08:44:28 +0200

    Sistemati i nomi delle activity, implementato ActivityRate.kt (ancora da collegare tutto)

--- app/src/main/java/com/example/myandroidapplication/viewModel/ActivityRate.kt
+++ app/src/main/java/com/example/myandroidapplication/viewModel/ActivityRate.kt
@@ -3,10 +3,18 @@ package com.example.myandroidapplication.viewModel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myandroidapplication.R
+import hotchemi.android.rate.AppRate
 
-class activity_rate : AppCompatActivity() {
+class ActivityRate : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rate)
+
+        AppRate.with(this)
+            .setInstallDays(1)
+            .setLaunchTimes(1)
+            .setRemindInterval(2)
+            .monitor()
+        AppRate.showRateDialogIfMeetsConditions(this)
     }
 }
