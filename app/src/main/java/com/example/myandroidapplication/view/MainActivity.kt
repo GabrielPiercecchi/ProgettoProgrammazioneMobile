7c23dbd8ba6246e356fb16ef9dbbb8f4109d342e
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2023-04-27 14:43:20 +0200

    Sistemata tutta la main activity e tutto quello che la riguarda

--- app/src/main/java/com/example/myandroidapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/myandroidapplication/view/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.example.myandroidapplication.view
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import android.view.MenuItem
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.myandroidapplication.R
+import com.example.myandroidapplication.viewModel.AboutFragment
+import com.example.myandroidapplication.viewModel.RateFragment
+import com.example.myandroidapplication.viewModel.SettingsFragment
+import com.example.myandroidapplication.viewModel.TutorialFragment
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
-import okhttp3.internal.http2.StreamResetException
 
 // Classe per la schermata di login
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
 
-    //private var drawerlayout: DrawerLayout
+    private lateinit var drawerlayout: DrawerLayout
+    private lateinit var toolbar: Toolbar
 
     // Variabile utilizzata per le autenticazioni Firebase
     private lateinit var mAuth: FirebaseAuth
@@ -23,7 +28,48 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         mAuth = FirebaseAuth.getInstance()
+
+        drawerlayout = findViewById(R.id.drawer_layout)
+
+        toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawerlayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerlayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SettingsFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_settings)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_settings -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SettingsFragment()).commit()
+            R.id.nav_rate -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, RateFragment()).commit()
+            R.id.nav_about -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, AboutFragment()).commit()
+            R.id.nav_tutorial -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, TutorialFragment()).commit()
+            R.id.nav_logout -> Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+        }
+        drawerlayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if(drawerlayout.isDrawerOpen(GravityCompat.START)){
+            drawerlayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
