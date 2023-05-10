bf03c3e951f60380b2b9f59aacdeac6d2d02b014
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2023-05-10 11:21:28 +0200

    Implementata la richiesta con OkHttp

--- app/src/main/java/com/example/myandroidapplication/view/LeaderboardsActivity.kt
+++ app/src/main/java/com/example/myandroidapplication/view/LeaderboardsActivity.kt
@@ -9,13 +9,18 @@ import com.example.myandroidapplication.util.Constants.Companion.PLAYERS_URL
 import com.example.myandroidapplication.util.Constants.Companion.API_KEY_G
 import com.example.myandroidapplication.model.Player
 import com.example.myandroidapplication.util.Constants.Companion.API_KEY
+import com.example.myandroidapplication.util.Constants.Companion.API_KEY_J
 import com.example.myandroidapplication.util.Constants.Companion.API_KEY_L
+import com.example.myandroidapplication.util.Constants.Companion.API_KEY_Q
 import com.example.myandroidapplication.viewModel.ApiInterface
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 class LeaderboardsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +31,7 @@ class LeaderboardsActivity : AppCompatActivity() {
         getPlayer()
     }
 
-    private fun getPlayer(){
+     /*private fun getPlayer(){
         // Creo il builder di retrofit con l'URL desiderato
         val retrofitBuilder = Retrofit.Builder()
             .baseUrl(PLAYERS_URL)
@@ -36,30 +41,64 @@ class LeaderboardsActivity : AppCompatActivity() {
 
 
         // Eseguo il metodo getPlayer() dell' ApiInterface
-        val retrofitData = retrofitBuilder.getPlayer("Bearer $API_KEY", "#2RYPJYY")
+        val retrofitData = retrofitBuilder.getPlayer("#2RYPJYY")
 //        val retrofitData = retrofitBuilder.getLeaderboard()
 
 
         // Costruzione della risposta
-        retrofitData.enqueue(object : Callback<List<Player>?> {
-            override fun onResponse(call: Call<List<Player>?>, response: Response<List<Player>?>) {
-                val responseBody = response.body()!!
+        retrofitData.enqueue(object : Callback<Player> {
+            override fun onResponse(call: Call<Player>, response: Response<Player>) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()!!
 
-
-                // Costruzione della string di risposta e applicazione della risposta alla vista
-                val myStringBuilder = StringBuilder()
-                for(myData in responseBody){
-                    myStringBuilder.append(myData.name)
-                    myStringBuilder.append("\n")
+                    // Costruzione della string di risposta e applicazione della risposta alla vista
+                    /*val myStringBuilder = StringBuilder()
+                    for (myData in responseBody) {
+                        myStringBuilder.append(myData.name)
+                        myStringBuilder.append("\n")
+                    }*/
+                    val txtId: TextView = findViewById(R.id.txtId)
+                    txtId.text = "ma salve"
+                } else {
+                    val txtId: TextView = findViewById(R.id.txtId)
+                    txtId.text = "diocane"
                 }
-                val txtId: TextView = findViewById(R.id.txtId)
-                txtId.text = myStringBuilder
             }
 
             // Se fallisce...
-            override fun onFailure(call: Call<List<Player>?>, t: Throwable) {
+            override fun onFailure(call: Call<Player>, t: Throwable) {
                 Log.d("MainActivity", "onFailure: "+t.message)
             }
         })
+    }*/
+
+    private fun getPlayer(){
+        val client = OkHttpClient()
+        val url = PLAYERS_URL
+        val request = Request.Builder()
+                            .url(url)
+                            .addHeader("authorization","Bearer $API_KEY_J")
+                            .build()
+        client.newCall(request).enqueue(object : okhttp3.Callback{
+            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+                if(response.isSuccessful) {
+                    runOnUiThread() {
+                        val txtId: TextView = findViewById(R.id.txtId)
+                        txtId.text = "ma salve"
+                    }
+                } else {
+                    runOnUiThread() {
+                        val txtId: TextView = findViewById(R.id.txtId)
+                        txtId.text = "Diocane"
+                    }
+                }
+            }
+
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
+                Log.d("MainActivity", "onFailure: "+e.message)
+            }
+        })
+
     }
+
 }
