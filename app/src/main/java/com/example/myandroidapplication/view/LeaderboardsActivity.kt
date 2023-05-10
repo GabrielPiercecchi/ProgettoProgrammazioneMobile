bb909edb2f3028ee702afb1ae9f6424d13d7b82a
Lorenzo Zulli <zullilorenzo.24@gmail.com>
2023-05-10 15:41:42 +0200

    Raffinata la richiesta con OkHttp

--- app/src/main/java/com/example/myandroidapplication/view/LeaderboardsActivity.kt
+++ app/src/main/java/com/example/myandroidapplication/view/LeaderboardsActivity.kt
@@ -1,26 +1,16 @@
 package com.example.myandroidapplication.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myandroidapplication.R
-import com.example.myandroidapplication.util.Constants.Companion.PLAYERS_URL
-import com.example.myandroidapplication.util.Constants.Companion.API_KEY_G
-import com.example.myandroidapplication.model.Player
-import com.example.myandroidapplication.util.Constants.Companion.API_KEY
 import com.example.myandroidapplication.util.Constants.Companion.API_KEY_J
-import com.example.myandroidapplication.util.Constants.Companion.API_KEY_L
-import com.example.myandroidapplication.util.Constants.Companion.API_KEY_Q
-import com.example.myandroidapplication.viewModel.ApiInterface
+import com.example.myandroidapplication.util.Constants.Companion.PLAYERS_URL
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
+import com.example.myandroidapplication.model.Player
 
 class LeaderboardsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -83,13 +73,22 @@ class LeaderboardsActivity : AppCompatActivity() {
             override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
                 if(response.isSuccessful) {
                     runOnUiThread() {
+                        val responseBody = response.body?.string()
+                        // Costruzione della string di risposta e applicazione della risposta alla vista
+                        val myStringBuilder = StringBuilder()
+                        if (responseBody != null) {
+                            for (myData in responseBody) {
+                                //myStringBuilder.append(myData.name)
+                                myStringBuilder.append("\n")
+                            }
+                        }
                         val txtId: TextView = findViewById(R.id.txtId)
-                        txtId.text = "ma salve"
+                        txtId.text = responseBody
                     }
                 } else {
                     runOnUiThread() {
                         val txtId: TextView = findViewById(R.id.txtId)
-                        txtId.text = "Diocane"
+                        txtId.text = "Non funziona!"
                     }
                 }
             }
@@ -98,7 +97,5 @@ class LeaderboardsActivity : AppCompatActivity() {
                 Log.d("MainActivity", "onFailure: "+e.message)
             }
         })
-
     }
-
 }
