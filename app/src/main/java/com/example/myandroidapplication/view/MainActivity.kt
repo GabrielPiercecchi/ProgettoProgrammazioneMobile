867e0ac81c9d9e55d523985c57bddf06074de9ff
GabrielP <S1097612@studenti.univpm.it>
2023-05-22 16:26:40 +0200

    Commit recupero progetto

--- app/src/main/java/com/example/myandroidapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/myandroidapplication/view/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private lateinit var bottomNavigationView: BottomNavigationView
 
     // Variabile utilizzata per le autenticazioni Firebase
-    //OK
+    //OK OK
     private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
